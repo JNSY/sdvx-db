2af0be9f6e996c6c984f1e43f3e4987c857db898
JNSY <jinsei1105@gmail.com>
2022-12-05 02:56:08 +0900

    fix firebaseライブラリ側の問題があるためポップアップログインに変更

--- functions/login.ts
+++ functions/login.ts
@@ -1,7 +1,7 @@
-import { GoogleAuthProvider, signInWithRedirect } from "firebase/auth";
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import { auth } from "../firebaseConfig";
 
 export const login = () => {
   const provider = new GoogleAuthProvider();
-  signInWithRedirect(auth, provider);
+  signInWithPopup(auth, provider);
 };
