ab17a020f3ef52f348a7d0ced480679964dd1457
JNSY <jinsei1105@gmail.com>
2022-12-17 19:17:09 +0900

    chore index.jsのHead関連を外だし

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,15 @@
 import { getAuth, onAuthStateChanged } from "firebase/auth";
-import Head from "next/head";
 import Image from "next/image";
 import { useEffect, useState } from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { createClient, Provider } from "urql";
 import { auth } from "../firebaseConfig";
 import Footer from "./footer";
+import HeadComp from "./head";
 import Header from "./header";
 import BottomAppBar from "./home/organisms/bottomAppBar";
 import BpmElements from "./home/organisms/bpmElements";
 import TopAppBar from "./home/organisms/topAppBar";
-
-const GA_TRACKING_ID = process.env.GA_TRACKING_ID || "";
 
 export default function Home() {
   const [user, loading, errorOfAuthState] = useAuthState(auth);
@@ -29,7 +27,6 @@ export default function Home() {
     },
   });
 
-  //MEMO:ログイン後、observeLoginUserが8回くらい一気に発火するのはなんでだろう
   const observeLoginUser = function (user: any) {
     //ログイン状態が変更された時の処理
     if (user) {
@@ -40,7 +37,6 @@ export default function Home() {
   };
 
   useEffect(() => {
-    /* 第1引数には実行させたい副作用関数を記述*/
     console.log("副作用関数が実行されました！");
     onAuthStateChanged(getAuth(), observeLoginUser);
   }, []);
@@ -48,43 +44,7 @@ export default function Home() {
   return (
     <Provider value={client}>
       <div>
-        <Head>
-          <title>SDVX BPM DB</title>
-          <link rel="icon" href="/favicon.ico" />
-          {GA_TRACKING_ID != null && (
-            <>
-              {/* Global Site Tag (gtag.js) - Google Analytics */}
-              <script
-                async
-                src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
-              />
-              <script
-                dangerouslySetInnerHTML={{
-                  __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', '${GA_TRACKING_ID}', {
-                page_path: window.location.pathname,
-              });`,
-                }}
-              />
-            </>
-          )}
-          <meta property="og:title" content="SDVX DB" />
-          <meta property="og:description" content="SDVX DB" />
-          {/* <meta
-          property="og:image"
-          content="../public/sdvx_bpm_database_logo_for_twitter.png"
-        /> */}
-          <meta
-            name="twitter:image"
-            content="../public/sdvx_bpm_database_logo_for_twitter.png"
-          />
-          <meta name="twitter:card" content="summary_large_image" />
-          <meta name="twitter:title" content="SDVX DB" />
-          <meta name="twitter:description" content="SDVX DB" />
-        </Head>
+        <HeadComp />
         <body>
           <TopAppBar />
           <div className="p-8 max-w-0-1">
